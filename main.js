8201014c71cd171643a744d7586f7bb9d123c0b0
Jacqueline <maaaander@gmail.com>
2020-09-11 15:29:17 -0400

    updated colors and video

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
 
 $(document).ready(function(){
-  $('.header').height($(window).height());
+    $('.header').height($(window).height());
+    //$('body').css( "maxWidth", ( $( window ).width())) 
 
 
 
@@ -9,53 +10,53 @@ $(document).ready(function(){
 var modal = document.getElementById('myModal');
 
 $(".img-thumbnail").on("click", function(e) {
-  const imgsrc = $(this).attr("id")
-  if (imgsrc.slice(-3) === "png") {
-      const caption =  $(this).attr("alt");
-      $('#myModal').empty().append('<img class="modal-content " id="modalImg" /> <figcaption class="figure-caption"><figcaption>');
-      $("#modalImg").attr("src", imgsrc);
-      $('.figure-caption').text(caption);
-      modal.style.display = "block";
-      }
-  else {
-      ($('#myModal').empty().append('<video class="modal-content figure-img " id="video" controls autoplay></video> <figcaption class="figure-caption"></figcaption>'));
-      ($("#video").append('<source id="vidSource" src="" type="video/mp4" ></source>'))
-      $("#vidSource").attr("src", imgsrc)
-      const caption =  $(this).attr("alt");
-      $('.figure-caption').text(caption);
-      modal.style.display = "block";
-  }
+    const imgsrc = $(this).attr("id")
+    if (imgsrc.slice(-3) === "png") {
+        const caption =  $(this).attr("alt");
+        $('#myModal').empty().append('<img class="modal-content " id="modalImg" /> <figcaption class="figure-caption"><figcaption>');
+        $("#modalImg").attr("src", imgsrc);
+        $('.figure-caption').text(caption);
+        modal.style.display = "block";
+        }
+    else {
+        ($('#myModal').empty().append('<video class="modal-content figure-img " id="video" autoplay></video> <figcaption class="figure-caption"></figcaption>'));
+        ($("#video").append('<source id="vidSource" src="" type="video/mp4" ></source>'))
+        $("#vidSource").attr("src", imgsrc)
+        const caption =  $(this).attr("alt");
+        $('.figure-caption').text(caption);
+        modal.style.display = "block";
+    }
 })
 
 //updates projects displayed depending on tag clicked
 $(".filter").on('click', function() {
-filterTerm = $(this).attr("id");
-if (!$(this).hasClass("clicked")){
-  $(this).addClass("clicked");
-  $(".filter").each(function(){
-    if ($(this).attr("id") === filterTerm) {
-      $(this).css("cssText", "background: rgb(234, 236, 193);")
-    }
-    else{
-      $(this).css("cssText", "background: rgb(255, 255, 255);")
-    }
-  });
+  filterTerm = $(this).attr("id");
+  if (!$(this).hasClass("clicked")){
+    $(this).addClass("clicked");
+    $(".filter").each(function(){
+      if ($(this).attr("id") === filterTerm) {
+        $(this).css("cssText", " opacity: 1")
+      }
+      else{
+        $(this).css("cssText", "opacity: 0.7")
+      }
+    });
+    $(".imgdiv").each(function() {
+      if (!$(this).hasClass(filterTerm)) {
+        $(this).css("cssText", "display: none !important")
+      }
+      else {
+        $(this).css("cssText", "display: show")
+      }
+    })
+  }
+else {
+  $(this).removeClass("clicked");
+  $("#" + filterTerm).css("cssText", "opacity: 0.7");
   $(".imgdiv").each(function() {
     if (!$(this).hasClass(filterTerm)) {
-      $(this).css("cssText", "display: none !important")
-    }
-    else {
-      $(this).css("cssText", "display: show")
-    }
-  })
-}
-else {
-$(this).removeClass("clicked");
-$("#" + filterTerm).css("cssText", "background: rgb(255, 255, 255)");
-$(".imgdiv").each(function() {
-  if (!$(this).hasClass(filterTerm)) {
-    $(this).css("cssText", "display: show");
-}}
+      $(this).css("cssText", "display: show");
+  }}
 )}
 })
 
@@ -68,14 +69,14 @@ var span = document.getElementsByClassName("modal")[0];
 
 // When the user clicks on span, close the modal
 span.onclick = function() { 
-  modal.style.display = "none";
+    modal.style.display = "none";
 }
 
 
 
 
-
-
+  
+  
 
 
 
